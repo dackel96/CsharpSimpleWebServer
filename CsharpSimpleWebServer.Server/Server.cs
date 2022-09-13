b9c62df61e27f86ca92e0fdbc9d2cfd71b61c271
Ivan Iliev <ivanilievdackel@gmail.com>
2022-09-13 18:51:16 +0300

    Initial commit

---
+++ CsharpSimpleWebServer.Server/Server.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpSimpleWebServer.Server
+{
+    public class Server
+    {
+        private readonly IPAddress ipAddress;
+
+        private readonly int port;
+
+        private readonly TcpListener tcpListener;
+        public Server(string ipAddress, int port)
+        {
+            this.ipAddress = IPAddress.Parse(ipAddress);
+
+            this.port = 8080;
+
+            this.tcpListener = new TcpListener(this.ipAddress, port);
+        }
+
+        public async Task Start()
+        {
+
+            this.tcpListener.Start();
+
+
+            Console.WriteLine("Server started...");
+
+            Console.WriteLine("Listening for requests....");
+
+            while (true)
+            {
+                TcpClient connection = await this.tcpListener.AcceptTcpClientAsync();
+
+                NetworkStream networkStream = connection.GetStream();
+
+                int bufferLength = 1024;
+
+                byte[] buffer = new byte[bufferLength];
+
+                StringBuilder requestBuilder = new StringBuilder();
+
+                while (networkStream.DataAvailable)
+                {
+                    var bytesReader = await networkStream.ReadAsync(buffer, 0, bufferLength);
+
+                    requestBuilder.Append(Encoding.UTF8.GetString(buffer, 0, bytesReader));
+                }
+
+                Console.WriteLine(requestBuilder);
+
+                string content = "Кааак е уе!!!";
+
+                int contentLength = Encoding.UTF8.GetByteCount(content);
+
+                string response = $@"HTTP/1.1 200 OK
+Server: My Web Server
+Date: {DateTime.UtcNow.ToString("r")}
+Content-Length: {contentLength}
+Content-Type: text/plain; charset=UTF8
+
+{content}";
+
+                var HttpResponsebytes = Encoding.UTF8.GetBytes(response);
+
+                await networkStream.WriteAsync(HttpResponsebytes);
+
+                connection.Close();
+            }
+        }
+    }
+}
---
+++ CsharpSimpleWebServer/StartUp.cs
@@ -0,0 +1,16 @@
+﻿// 127.0.0.1 <-- localhost Default
+
+// Http://localhost:6969
+
+// TcpClient/Listener is for bytes transfer
+
+//GetStream flow of bytes
+
+using CsharpSimpleWebServer.Server;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+var server = new Server("127.0.0.1", 8080);
+
+await server.Start();
